9645d03109e98992daf57a05ea63a508fbb03b22
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-01-26 15:02:53 +0530

    Update index.js

--- near-webapp/src/components/Navbar/index.js
+++ near-webapp/src/components/Navbar/index.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const drawerWidth = 240;
 
@@ -11,10 +14,15 @@ export default function Navbar() {
         position="fixed"
         sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
       >
-        <Toolbar>
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
           <Typography variant="h6" noWrap component="div">
-            NearBot
+            NearBot Web Dashboard
           </Typography>
+          <Tooltip title="Open settings">
+            <IconButton sx={{ p: 0 }}>
+              <AccountCircleIcon style={{color: 'white'}} />
+            </IconButton>
+          </Tooltip>
         </Toolbar>
       </AppBar>
   );
