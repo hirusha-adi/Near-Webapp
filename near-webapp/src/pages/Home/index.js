000b840d98f57b50cf3bd31844badaaf725b7e67
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-01-26 14:26:04 +0530

    basic setup

---
+++ near-webapp/src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar'
+
+function Home() {
+  return (
+    <>
+      <Navbar/>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of the Near Web app.</p>
+    </>
+  );
+}
+
+export default Home;
