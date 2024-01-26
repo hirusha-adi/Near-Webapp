9ac65c83c8bd45c8ba44c82cc2690a8024ad2d1f
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2024-01-26 14:46:02 +0530

    enable /music route

--- near-webapp/src/App.js
+++ near-webapp/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Music from './pages/Music';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/music" element={<Music />} /> */}
+        <Route path="/music" element={<Music />} />
       </Routes>
     </Router>
   );
